38b10c231af1e78e7d94ac3defbbc9840a6206bb
Michael Rawlings <rawlingsm99@gmail.com>
2022-01-20 17:42:21 -0600

    made inline row for the five day forcast

--- script.js
+++ script.js
@@ -1,114 +1,136 @@
+const apiKey = "c439a2b6de2ccb2a87cb0290ffef83a0";
+let searchedCities = JSON.parse(localStorage.getItem("searched-cities")) || [];
+var currentWeather = document.getElementById("current-weather");
+let fiveDayWeather = document.getElementById("five-day-weather");
+let city;
+let lat;
+let lon;
+
 async function getCity() {
   var form = document.querySelector("form");
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    var city = document.querySelector("form").firstElementChild.value;
+    city = document.querySelector("form").firstElementChild.value;
     getWeather();
   });
 }
 getCity();
 
 async function getWeather() {
-  const apiKey = "c439a2b6de2ccb2a87cb0290ffef83a0";
-  var wlocation = document.getElementById("w-location");
-  var wdesc = document.getElementById("w-desc");
-  var wstring = document.getElementById("w-string");
-  var wicon = document.getElementById("w-icon");
-  var currentWeather = document.getElementById("current-weather");
-  var city = document.querySelector("form").firstElementChild.value;
-  // city = city.value;
-  
-
   var response = await fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
   );
   var weatherData = await response.json();
   // variables for city latitude and longitude information.
-  const lat = weatherData.city.coord.lat;
-  const lon = weatherData.city.coord.lon;
+  lat = weatherData.city.coord.lat;
+  lon = weatherData.city.coord.lon;
 
-  var res = await fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=imperial`
-  );
+  generalWeather();
+  cityData();
+}
 
-  var weather = await res.json();
-  
+function retrieveData() {
+  var viewedCities = document.getElementById("searched-cities");
 
-  // This is for the current day's weather (day)
-  currentWeather.innerHTML = `<h1 id="w-location">${city} and ${weather.current.dt}</h1>
-  <h3 class="text-dark" id="w-desc">${weather.current.weather[0].description}</h3>
-  <p id="w-string">${weather.current.temp}</p>
-  <p>Wind ${weather.current.wind_deg}</p>
-  <p>Humidity ${weather.current.humidity}</p>
-  <p>UV Index ${weather.current.uvi}</p>
-  <img id="w-icon" style = "width: 100px" src="https://openweathermap.org/img/w/${weather.current.weather[0].icon}.png"/>`;
+  let location = "";
 
-  // This is for the five day forcast
-
-  var currentWeather = document.getElementById("current-weather");
-  let fiveDayWeather = document.getElementById("five-day-weather");
-  let day = "";
-  const dailies = weather.daily;
-  
-  dailies.pop();
-  dailies.pop();
-  dailies.pop();
-  
-
-  dailies.forEach((daily) => {
-    day += 
-    `<h1 id="w-location">${daily.dt}</h1>
-  <h3 class="text-dark" id="w-desc">${daily.weather[0].description}</h3>
-  <p id="w-string">${daily.temp.day}</p>
-  <p>Wind ${daily.wind_deg}</p>
-  <p>Humidity ${daily.humidity}</p>
-  <p>UV Index ${daily.uvi}</p>
-  <img id="w-icon" style = "width: 100px" src="https://openweathermap.org/img/w/${daily.weather[0].icon}.png"/>`;
+  searchedCities.forEach((place) => {
+    location += `<li>
+      <button class="city-button" type="submit">${place.city}</button>
+      <div class="lat-button">${place.lat}</div>
+      <div class="lon-button">${place.lon}</div>
+    </li>`;
   });
 
-  fiveDayWeather.innerHTML = day;
-  
-  // This is the code that stores the data into local memory.
+  viewedCities.innerHTML = location;
+}
+retrieveData();
+
+// This is the code that stores the data into local memory.
+function cityData() {
   var addEntry = {
-    "lat": lat,
-    "lon": lon,
-    "city": city,
+    lat: lat,
+    lon: lon,
+    city: city,
   };
 
-  
-  searchedCities = JSON.parse(localStorage.getItem("searched-cities")) || [];
-
-  cityExists = searchedCities.some(obj => obj.city === addEntry.city);
+  cityExists = searchedCities.some((obj) => obj.city === addEntry.city);
 
   if (!cityExists) {
     localStorage.setItem("addEntry", JSON.stringify(addEntry));
     searchedCities.push(addEntry);
     localStorage.setItem("searched-cities", JSON.stringify(searchedCities));
   }
-
 }
-function retrieveData() {
 
-  
-  let existingCities = JSON.parse(localStorage.getItem("searched-cities"));
-console.log(existingCities[0].city);
-  var searchedCities = document.getElementById("searched-cities");
-   
-let location="";
+firstSearch();
 
- existingCities.forEach((place) => {
-    location += 
-    `<li>
-    hello
-      <button>${place.city}</button>
-      <div class="lat-button">${place.lat}</div>
-      <div class="lon-button">${place.lon}</div>
-    </li>`
-    
-    
+async function firstSearch() {
+  if (searchedCities) {
+    city = searchedCities[0].city;
+    lat = searchedCities[0].lat;
+    lon = searchedCities[0].lon;
+
+    generalWeather();
+  }
+}
+
+async function generalWeather() {
+  var res = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=imperial`
+  );
+
+  var weather = await res.json();
+
+  // This is for the current day's weather (day)
+  currentWeather.innerHTML = `<div class="card-title">${city}</div>  
+  <h1 id="w-location">${weather.current.dt}</h1>
+<h3 class="text-dark" id="w-desc">${weather.current.weather[0].description}</h3>
+<p id="w-string">${weather.current.temp}</p>
+<p>Wind ${weather.current.wind_deg}</p>
+<p>Humidity ${weather.current.humidity}</p>
+<img id="w-icon" style = "width: 100px" src="https://openweathermap.org/img/w/${weather.current.weather[0].icon}.png"/>`;
+
+  // This is for the five day forcast
+
+  let day = "";
+  const dailies = weather.daily;
+
+  dailies.pop();
+  dailies.pop();
+  dailies.pop();
+
+  dailies.forEach((daily) => {
+    day += `
+    <div class="col-md-2"><h1 id="w-location">${daily.dt}</h1>
+<h3 class="text-dark" id="w-desc">${daily.weather[0].description}</h3>
+<p id="w-string">${daily.temp.day}</p>
+<p>Wind ${daily.wind_deg}</p>
+<p>Humidity ${daily.humidity}</p>
+<p class="uv-index">UV Index <span class="uv-number">${daily.uvi}</span></p>
+<img id="w-icon" style = "width: 100px" src="https://openweathermap.org/img/w/${daily.weather[0].icon}.png"/>
+</div>`;
   });
- 
-searchedCities.innerHTML=location  
-console.log(location);
+
+  fiveDayWeather.innerHTML = day;
 }
-retrieveData()
+
+function retLatLon() {
+  let previousCities = document.querySelectorAll(".city-button");
+
+  previousCities.forEach((indivCity) => {
+    indivCity.addEventListener("click", (event) => {
+      // info coming from the DOM
+      let latSearched = indivCity.nextElementSibling;
+      let lonSearched = latSearched.nextElementSibling;
+
+      city = indivCity.innerText;
+      lat = latSearched.innerText;
+      lon = lonSearched.innerText;
+      console.log(city, lat, lon);
+
+      generalWeather();
+    });
+  });
+}
+retLatLon();
